a2ac77404b7c567ebc8380b6852eb3008841488f
ferwanguer <f.wguerra@outlook.com>
2025-07-10 20:44:18 +0200

    namechange

--- tinyfal/android/app/build.gradle.kts
+++ tinyfal/android/app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.tinyfal"
+    namespace = "com.tinyfal.tinyfal"
     compileSdk = flutter.compileSdkVersion
     // ndkVersion = flutter.ndkVersion
     ndkVersion = "27.0.12077973"
@@ -25,7 +25,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.tinyfal"
+        applicationId = "com.tinyfal.tinyfal"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         //minSdk = flutter.minSdkVersion
